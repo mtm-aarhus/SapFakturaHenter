649811034d91924239e2b0e9e643adee5e970255
Laura-Salmonsen <balas@aarhus.dk>
2026-09-15 08:56:04 +0200

    comma

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -193,7 +193,7 @@ def process(orchestrator_connection: OrchestratorConnection) -> None:
     runs = [
         {"RunName": 'SD løn udtræk', "UploadMappe": "SP"},
         {"RunName": "MTMIkkeGodkendteTimer", "UploadMappe": "SP"},
-        {"RunName": "ZPSA_Brugerparametre", "UploadMappe": "SP"}
+        {"RunName": "ZPSA_Brugerparametre", "UploadMappe": "SP"},
         {"RunName": "SD Forfaldne faktura", "UploadMappe": "SP"},
         {"RunName": "SD Stamdatatabel", "UploadMappe": "SP"},
         {"RunName": "SDAfstemning", "UploadMappe": "SP"},
